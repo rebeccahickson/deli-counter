603a15f0fc2d8ea3b5a487816520b50040da0ae3
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2020-10-25 15:37:03 -0400

    commit tree 6ef6bd19db22b4409d9044ed1317e20f68db6872

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,18 @@
-# Write your code here.
+def line(katz_deli)
+    if katz_deli.size == 0
+        puts "The line is currently empty."
+    else
+        string = ""
+        katz_deli.each_with_index { | element, index | string << " #{index + 1}. #{element}" }
+        puts "The line is currently:#{string}"
+    end
+end
+
+def take_a_number(katz_deli, name)
+    katz_deli << name
+    puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
+end
+
+def now_serving(katz_deli)
+    katz_deli.size == 0 ? (puts "There is nobody waiting to be served!") : (puts "Currently serving #{katz_deli.shift}.")
+end 
